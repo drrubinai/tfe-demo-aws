5b220bf513fe8ad08c0a73d867a14674b2570270
Irfan Ilyas <Irfan.Ilyas@cantire.com>
2021-03-19 12:10:54 -0400

    getting ready for apply.

--- sg-aws.tf
+++ sg-aws.tf
@@ -1,54 +1,28 @@
-resource "aws_security_group" "sg-00c11fc6eb8b0e3e2" {
-     description   = "Security group for AEM author nodes"
-     egress        = [
-        {
-            cidr_blocks      = [
-                "0.0.0.0/0",
-            ]
-            description      = ""
-            from_port        = 0
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "-1"
-            security_groups  = []
-            self             = false
-            to_port          = 0
-        },
-    ]
-     ingress       = []
-     name          = "author-aem"
-     tags          = {
-        "Environment" = "staging"
-        "Name"        = "author-aem"
-    }
-    timeouts {}
-}
 
+resource "aws_security_group" "sg-032021d1ce1f029fdab2e" {
+  description = "Allow traffic to jmx from Zabbix server"
+  egress      = []
 
-resource "aws_security_group" "sg-00d1ce1f029fdab2e" {
-     description   = "Allow traffic to jmx from Zabbix server"
-     egress        = []
+  ingress = [
+    {
+      cidr_blocks = [
+        "127.0.0.1/32",
+      ]
+      description      = ""
+      from_port        = 16302
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 16302
+    },
+  ]
+  name = "author-aem-jmx"
+  tags = {
+    "Environmnet" = "development"
+    "Name"        = "author-aem-jmx"
+  }
 
-     ingress       = [
-        {
-            cidr_blocks      = [
-                "0.0.0.0/0",
-            ]
-            description      = ""
-            from_port        = 16302
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "tcp"
-            security_groups  = []
-            self             = false
-            to_port          = 16302
-        },
-    ]
-     name          = "author-aem-jmx"
-     tags          = {
-        "Environmnet" = "development"
-        "Name"        = "author-aem-jmx"
-    }
-
-    timeouts {}
+  timeouts {}
 }
